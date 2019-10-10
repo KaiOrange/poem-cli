c108adeaf8a594cb9361945d390870feaf3220ec
Orange <huangkaiking@126.com>
2019-10-10 17:48:19 +0800

    TOKEN不再定死

--- src/random-poem.js
+++ src/random-poem.js
@@ -1,17 +1,45 @@
 "use strict"
 const axios = require('axios');
-const TOKEN = 'DGfXPJmVnrpMe7NW5QRV2s6tgbmtI94U';
+const fs = require('fs');
+const path = require('path');
 
-module.exports = function (){
-  return new Promise(function (resolve, reject) {
-    // 旧的接口：'https://api.gushi.ci/all.json'
-    axios.get('https://v2.jinrishici.com/sentence',{
+let TOKEN = null;
+let fileName = path.join(__dirname,'..', 'token.json');
+
+function fetchData(resolve,reject){
+  axios.get('https://v2.jinrishici.com/sentence',{
       headers: { 'X-User-Token': TOKEN },
     })
     .then(function (response) {
       resolve(response.data || {})
     }).catch(function (error){
       reject(error)
+    });
+}
+
+module.exports = function (){
+  return new Promise(function (resolve, reject) {
+    // 旧的接口：'https://api.gushi.ci/all.json'
+    fs.exists(fileName, function (exists) {
+      if (!exists) {
+        axios.get('https://v2.jinrishici.com/token').then(function (response) {
+          TOKEN = response.data.data
+          fs.writeFile(
+            fileName,
+            JSON.stringify(response.data),
+            err => {
+              if (err) throw err;
+              fetchData(resolve, reject)
+            }
+          );
+        })
+      } else {
+        fs.readFile(fileName, { encoding: 'utf8' }, (err, data) => {
+          if (err) throw err;
+          TOKEN = JSON.parse(data).data;
+          fetchData(resolve, reject)
+        });
+      }
     });
   });
 }
