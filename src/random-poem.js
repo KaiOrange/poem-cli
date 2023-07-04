a0f9713f23230080bd5257d09499fb8a2ddb82de
huangkai <huangkai@acoinfo.com>
2023-07-04 20:09:28 +0800

    feat: URL修改

--- src/random-poem.js
+++ src/random-poem.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 module.exports = function (){
   return new Promise(function (resolve, reject) {
-    axios.get('https://api.gushi.ci/all.json')
+    axios.get('https://v1.jinrishici.com/all.json')
     .then(function (response) {
       resolve(response.data || {})
     }).catch(function (error){
